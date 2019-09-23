69fae54b396c6e4b9f87ff36a046771e5f857fff
yu <66969721@qq.com>
2019-09-23 11:37:50 +0800

    code comments

--- http_server.go
+++ http_server.go
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) XDE, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 package main
 
 import (
@@ -12,19 +19,23 @@ import (
 	"google.golang.org/grpc"
 )
 
+// RPC服务器地址配置
 const (
-	RPC_ADDR = "127.0.0.1:8082"
+	RPCAddr = "127.0.0.1:8082" // rpc address
 )
 
+// 事件代号代码
 const (
-	LOING_EVENT = 1
-	CHAT_EVENT  = 2
+	LoginEvent = 1 // 登录事件
+	chatEvent  = 2 // 聊天事件
 )
 
-type RpcServer struct{}
+//RPCServer 用于调用rpc服务
+type RPCServer struct{}
 
-func (s *RpcServer) rpc(event int, i interface{}) (*miliao.RPCResult, error) {
-	conn, err := grpc.Dial(RPC_ADDR, grpc.WithInsecure())
+// rpc服务，根据事件调用不同的rpc
+func (s *RPCServer) rpc(event int, i interface{}) (*miliao.RPCResult, error) {
+	conn, err := grpc.Dial(RPCAddr, grpc.WithInsecure())
 	if err != nil {
 		return nil, err
 	}
@@ -37,12 +48,12 @@ func (s *RpcServer) rpc(event int, i interface{}) (*miliao.RPCResult, error) {
 	var rpcErr error
 
 	switch event {
-	case LOING_EVENT:
+	case LoginEvent:
 		user, _ := i.(miliao.User)
 		res, _ := t.Login(context.Background(), &user)
 		fmt.Printf("LOGIN:%s %s\n", user.GetName(), user.GetPassword())
 		result = *res
-	case CHAT_EVENT:
+	case chatEvent:
 		msg, _ := i.(miliao.ChatMsg)
 		res, _ := t.Chat(context.Background(), &msg)
 		result = *res
@@ -60,10 +71,11 @@ func (s *RpcServer) rpc(event int, i interface{}) (*miliao.RPCResult, error) {
 	return &result, nil
 }
 
+// http请求处理，根据event进行rpc路由
 func httpRequest(w *http.ResponseWriter, event int, i interface{}) {
 	var res miliao.RPCResult
 
-	var server = &RpcServer{}
+	var server = &RPCServer{}
 	result, err := server.rpc(event, i)
 
 	if err == nil {
@@ -80,29 +92,27 @@ func httpRequest(w *http.ResponseWriter, event int, i interface{}) {
 	fmt.Fprint(*w, string(ret))
 }
 
-// http login handle
+// 登录handle
 func login(w http.ResponseWriter, r *http.Request) {
 	body, _ := ioutil.ReadAll(r.Body)
 	var user miliao.User
 	if err := json.Unmarshal(body, &user); err == nil {
-		httpRequest(&w, LOING_EVENT, user)
+		httpRequest(&w, LoginEvent, user)
 	}
 }
 
-// http chat handle
+// 聊天handle
 func chat(w http.ResponseWriter, r *http.Request) {
 	body, _ := ioutil.ReadAll(r.Body)
 	var msg miliao.ChatMsg
 	if err := json.Unmarshal(body, &msg); err == nil {
-		httpRequest(&w, CHAT_EVENT, msg)
+		httpRequest(&w, chatEvent, msg)
 	}
 }
 
 func main() {
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/chat", chat)
-
-	fmt.Println("------ http json server --------")
 
 	if err := http.ListenAndServe("0.0.0.0:8080", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
